a5e9f1498b2c4fade7a2b7e9f7617f79b42a4d4c
subratah01 <subratah01@gmail.com>
2024-11-15 22:34:34 +0530

    Flutter code for the UI with “AppBar, IconButton, CircleAvatar, Text, Icon” widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  //Flutter code for the UI with “AppBar, IconButton, CircleAvatar, Text, Icon” widgets
   runApp(const MyApp());
 }
 
@@ -11,9 +12,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, Flutter!'),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          'Need Blood',
+          style: TextStyle(
+            fontSize: 23,
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('Add button pressed');
+            },
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 90,
+              backgroundColor: Color.fromRGBO(117, 113, 117, 1),
+              child: Icon(
+                Icons.bloodtype_outlined,
+                size: 100,
+                color: Colors.red,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Donate Blood',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ],
         ),
       ),
     );
